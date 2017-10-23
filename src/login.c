2828c7a4280295aae723f08c9bce7ba2111ade80
Abdullah ibn Nadjo <abdullahibnnadjo@gmail.com>
2017-10-23 15:51:05 +0200

    Make xorg login works for root user

--- src/login.c
+++ src/login.c
@@ -555,7 +555,14 @@ int init_env(pam_handle_t* pam_handle, struct passwd* pw)
 	setenv("USER", pw->pw_name, 1);
 	setenv("SHELL", pw->pw_shell, 1);
 	setenv("LOGNAME", pw->pw_name, 1);
-	snprintf(tmp, sizeof(tmp), "/home/%s/%s", pw->pw_name, LY_XAUTHORITY);
+	if(strcmp("root", pw->pw_name) == 0)
+	{
+		snprintf(tmp, sizeof(tmp), "/root/%s", LY_XAUTHORITY);
+	}
+	else
+	{
+		snprintf(tmp, sizeof(tmp), "/home/%s/%s", pw->pw_name, LY_XAUTHORITY);
+	}
 	setenv("XAUTHORITY", tmp, 0);
 	buf = termenv ? strdup(termenv) : NULL;
 	setenv("TERM", buf ? buf : "linux", 1);
